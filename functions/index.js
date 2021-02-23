aa0cd0e94e10b936d6a07653dca3b2300b5c2170
Aptidon Moussa Ali <aptidonloko@gmail.com>
2021-02-23 16:35:54 +0100

    Creationg CRUD methods

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,3 @@
-/* eslint-disable prefer-const */
-const functions = require('firebase-functions');
-const express = require('express');
-// const cors = require('cors');
-const admin = require('firebase-admin');
-
-const app = express();
-
-admin.initializeApp();
-
-app.get('/', async (req, res) => {
-  const snapshot = await admin.firestore().collection('users').get();
-
-  const users = [];
-  snapshot.forEach((doc) => {
-    let id = doc;
-    let data = doc.data();
-
-    users.push({id, ...data});
-  });
-
-  res.status(200).send(JSON.stringify(users));
-});
-
-app.post('/', async (req, res) => {
-  const user = req.body;
-
-  await admin.firestore().collection('users').add(user);
-
-  res.status(201).send();
-});
-
-exports.user = functions.https.onRequest(app);
+module.exports = {
+  ...require('./controllers/user'),
+};
